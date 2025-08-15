aad232b2ef8c0db444146062ba3d25c0236ca6e4
Derrick Wafula <derrickwafula1500@gmail.com>
2025-08-15 15:39:22 +0300

    Removed dist files

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { CurlUITag, CurlUIElementProps, CurlUIChildComponent, CurlUIRenderElement, CurlUIComponentProps, CurlUIComponent, CurlUIStore, CurlUIStoreState } from "./types";
-export declare function CreateElement(tag: CurlUITag, properties: CurlUIElementProps, ...children: Array<CurlUIChildComponent>): CurlUIRenderElement;
-export declare function CreateComponent(properties: CurlUIComponentProps): CurlUIComponent;
-export declare function InstanceReference(): {
-    isInstanceReference: boolean;
-    instance: null;
-};
-export declare function Render(element: CurlUIRenderElement, htmlElement: HTMLElement): void;
-export declare function Store(defaultState: CurlUIStoreState): CurlUIStore;
