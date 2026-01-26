c7e34fca2d84bca244ea19a4894d7d75df2930bc
Derrick Wafula <derrickwafula1500@gmail.com>
2026-01-26 07:23:35 +0300

    Updated unique ID generation

--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,11 @@ import {
 
 /**
  * Generates a supposedly unique string.
- * @returns A "unique" string : I HAVE NO GUARANTEE FOR THIS. It just happens to be sufficient for this case.
+ * @returns A "unique" string : I HAVE NO GUARANTEE FOR THIS.
+ * It just happens to be sufficient for this case.
  */
-function GetUniqueId_(): string {
-    let x = 0;
-
-    for (let q = 0; q < 10; ++q) {
-        x += Date.now() * Math.random();
-    }
-
-    return x.toString().replace(".", "");
+function getUniqueId(): string {
+    return crypto.randomUUID();
 }
 
 /**
@@ -32,7 +27,7 @@ function GetUniqueId_(): string {
  * @param tag The tag to check
  * @returns Whether its an svg tag
  */
-function IsSvgTag_(tag: string = ""): boolean {
+function isSvgTag(tag: string = ""): boolean {
     return [
         "svg",
         "path",
@@ -49,7 +44,7 @@ function IsSvgTag_(tag: string = ""): boolean {
  * Allowed element attributes.
  * Used to check valid vlues when creating an HTML element.
  */
-const ValidElementAttributes_ = [
+const ValidElementAttributes = [
     // Global Attributes
     "accesskey",
     "autocapitalize",
@@ -225,13 +220,14 @@ const ValidElementAttributes_ = [
 ];
 
 /**
- * Checks if an element property(its name and value), are valid HTML element attributes.
+ * Checks if an element property(its name and value),
+ * are valid HTML element attributes.
  * @param key Name of the attribute
  * @param value Value of the attribute
  * @returns Whether it is a valid attribute.
  */
-function IsValidElementProperty_(key: string, value: any): boolean {
-    if (ValidElementAttributes_.includes(key) || key.startsWith("data-")) {
+function isValidElementProperty(key: string, value: any): boolean {
+    if (ValidElementAttributes.includes(key) || key.startsWith("data-")) {
         return typeof value !== "function" && typeof value !== "object";
     }
 
@@ -241,7 +237,7 @@ function IsValidElementProperty_(key: string, value: any): boolean {
 /**
  * Allowed event names for HTML elements.
  */
-const Events_: Array<string> = [
+const Events: Array<string> = [
     "onabort",
     "onafterprint",
     "onanimationcancel",
@@ -358,8 +354,8 @@ const Events_: Array<string> = [
  * @param value Listener function.
  * @returns
  */
-function IsEventListener_(key: string, value: any) {
-    return Events_.includes(key.toLowerCase()) && typeof value == "function";
+function isEventListener(key: string, value: any) {
+    return Events.includes(key.toLowerCase()) && typeof value == "function";
 }
 
 /**
@@ -367,175 +363,18 @@ function IsEventListener_(key: string, value: any) {
  * @param items The array to flatten
  * @returns The flattened array.
  */
-function Spread_(items: Array<any>): Array<any> {
-    let spread: Array<any> = [];
+function spread(items: Array<any>): Array<any> {
+    let spread_: Array<any> = [];
 
     items.map((x) => {
         if (Array.isArray(x)) {
-            spread = spread.concat(Spread_(x));
+            spread_ = spread_.concat(spread(x));
         } else {
-            spread.push(x);
+            spread_.push(x);
         }
     });
 
-    return spread;
-}
-
-/**
- * Creates a CurlUIRenderElement, a wrapper oven an element CurlUI can render on the DOM.
- * @param tag The element tag
- * @param properties Attributes to assign to the element
- * @param children The elements child elements.
- * @returns A CurlUIRenderElement.
- */
-function CreateElement_(
-    tag: CurlUITag,
-    properties: CurlUIElementProps<CurlUINativeElement>,
-    ...children: Array<CurlUIChildComponent | Array<CurlUIChildComponent>>
-): CurlUIRenderElement {
-    let spreadChildren: Array<CurlUIChildComponent> = Spread_(children);
-
-    if (properties.children) {
-        if (Array.isArray(properties.children)) {
-            spreadChildren = Spread_([
-                ...spreadChildren,
-                ...Spread_(properties.children),
-            ]);
-        } else {
-            spreadChildren = Spread_([
-                ...spreadChildren,
-                ...Spread_([properties.children]),
-            ]);
-        }
-
-        delete properties.children;
-    }
-
-    let loadedChildren: Array<CurlUIElement> = [];
-
-    for (let i = 0; i < spreadChildren.length; ++i) {
-        let child: CurlUIChildComponent = spreadChildren[i];
-
-        if (
-            typeof child === "string" ||
-            typeof child === "number" ||
-            typeof child === "boolean"
-        ) {
-            let wrapped: CurlUIElement = CreateElement_("span", {});
-            wrapped.element.textContent = child.toString();
-
-            loadedChildren.push(wrapped);
-        } else {
-            if (child) {
-                loadedChildren.push(child);
-            }
-        }
-    }
-
-    let element: HTMLElement | SVGElement = IsSvgTag_(tag)
-        ? document.createElementNS("http://www.w3.org/2000/svg", tag)
-        : document.createElement(tag);
-
-    let elementWrapper: CurlUIElement = {
-        isElement: true,
-        elementId: GetUniqueId_(),
-        element: element,
-        parent: null,
-        children: loadedChildren,
-        __mounted__: false,
-        setParent(parent) {
-            this.parent = parent;
-        },
-        _mounting_() {
-            this.mounting?.();
-            this.children?.map((child) => {
-                child._mounting_.bind(child)();
-            });
-        },
-        async _mounted_() {
-            this.__mounted__ = true;
-            this.mounted?.();
-            this.children?.map((child) => {
-                child._mounted_.bind(child)();
-            });
-        },
-        _unmounting_() {
-            this.unmounting?.();
-            this.children?.map((child) => {
-                child._unmounting_.bind(child)();
-            });
-        },
-        async _unmounted_() {
-            this.__mounted__ = false;
-            this.unmounted?.();
-            this.children?.map((child) => {
-                child._unmounted_.bind(child)();
-            });
-        },
-    };
-
-    if (properties.className) {
-        let classes = properties.className
-            .split(" ")
-            .map((t) => {
-                return t.trim();
-            })
-            .filter((t) => {
-                return t.length > 0;
-            });
-
-        element.classList.add(...classes);
-    }
-
-    if (properties.class) {
-        let classes = properties.class
-            .split(" ")
-            .map((t) => {
-                return t.trim();
-            })
-            .filter((t) => {
-                return t.length > 0;
-            });
-
-        element.classList.add(...classes);
-    }
-
-    if (properties.style) {
-        Object.assign(element.style, properties.style);
-    }
-
-    if (properties.instanceReference) {
-        properties.instanceReference.instance = element;
-    }
-
-    Object.keys(properties).map((key) => {
-        if (
-            ["instanceReference", "style", "className", "class"].includes(key)
-        ) {
-            return;
-        }
-
-        if (IsEventListener_(key, properties[key])) {
-            element.addEventListener(
-                key.slice(2).toLowerCase(),
-                properties[key]
-            );
-        } else if (IsValidElementProperty_(key, properties[key])) {
-            try {
-                element.setAttribute(key, properties[key]);
-            } catch (e) {
-                //
-                console.error(e);
-            }
-        }
-    });
-
-    loadedChildren.map((child) => {
-        child.setParent(elementWrapper);
-        element.appendChild(child.element);
-    });
-
-    return elementWrapper;
+    return spread_;
 }
 
 /**
@@ -543,35 +382,16 @@ function CreateElement_(
  * @param properties The component creation properties.
  * @returns The wrapped component template
  */
-function WrapComponent_(
-    properties: CurlUIComponentProps
+function wrapComponent(
+    properties: CurlUIComponentProps,
 ): CurlUIWrappedComponent {
     let component: CurlUIWrappedComponent = {
         ...properties,
         isComponent: true,
-        componentId: GetUniqueId_(),
+        componentId: getUniqueId(),
     };
 
     return component;
-}
-
-/**
- * Generates a function for custom element creation. From a wrapped custom component properties.
- * @param properties The custom component's properties.
- * @returns An element constructor function that takes props.
- */
-function CreateComponent_(properties: CurlUIComponentProps): CurlUIComponent {
-    let component: CurlUIWrappedComponent = WrapComponent_(properties);
-
-    let wrapper: CurlUIComponent = function (
-        props: CurlUIElementProps<CurlUINativeElement>
-    ): CurlUIRenderElement {
-        return CreateComponentInstance_(component, props);
-    };
-
-    Object.assign(wrapper, component);
-
-    return wrapper;
 }
 
 /**
@@ -580,16 +400,16 @@ function CreateComponent_(properties: CurlUIComponentProps): CurlUIComponent {
  * @param properties Props to pass to the custom component's constructor.
  * @returns A component instance, which is renderable to the DOM
  */
-function CreateComponentInstance_(
+function createComponentInstance(
     component: CurlUIWrappedComponent,
-    properties: CurlUIElementProps<CurlUINativeElement>
+    properties: CurlUIElementProps<CurlUINativeElement>,
 ): CurlUIComponentInstance {
-    let blankElement = CreateElement_("div", {});
+    let blankElement = CreateElement("div", {});
 
     let instance: CurlUIComponentInstance = {
         ...blankElement,
         ...component,
-        instanceId: GetUniqueId_(),
+        instanceId: getUniqueId(),
         state: {},
         props: {},
         getState() {
@@ -674,11 +494,206 @@ function CreateComponentInstance_(
 }
 
 /**
+ * Creates a CurlUIRenderElement, a wrapper oven an element
+ * CurlUI can render on the DOM.
+ * @param tag The element tag
+ * @param properties Attributes to assign to the element
+ * @param children The elements child elements.
+ * @returns A CurlUIRenderElement.
+ */
+export function CreateElement(
+    tag: CurlUITag,
+    properties: CurlUIElementProps<CurlUINativeElement>,
+    ...children: Array<CurlUIChildComponent | Array<CurlUIChildComponent>>
+) {
+    let spreadChildren: Array<CurlUIChildComponent> = spread(children);
+
+    if (properties.children) {
+        if (Array.isArray(properties.children)) {
+            spreadChildren = spread([
+                ...spreadChildren,
+                ...spread(properties.children),
+            ]);
+        } else {
+            spreadChildren = spread([
+                ...spreadChildren,
+                ...spread([properties.children]),
+            ]);
+        }
+
+        delete properties.children;
+    }
+
+    let loadedChildren: Array<CurlUIElement> = [];
+
+    for (let i = 0; i < spreadChildren.length; ++i) {
+        let child: CurlUIChildComponent = spreadChildren[i];
+
+        if (
+            typeof child === "string" ||
+            typeof child === "number" ||
+            typeof child === "boolean"
+        ) {
+            let wrapped: CurlUIElement = CreateElement("span", {});
+            wrapped.element.textContent = child.toString();
+
+            loadedChildren.push(wrapped);
+        } else {
+            if (child) {
+                loadedChildren.push(child);
+            }
+        }
+    }
+
+    let element: HTMLElement | SVGElement = isSvgTag(tag)
+        ? document.createElementNS("http://www.w3.org/2000/svg", tag)
+        : document.createElement(tag);
+
+    let elementWrapper: CurlUIElement = {
+        isElement: true,
+        elementId: getUniqueId(),
+        element: element,
+        parent: null,
+        children: loadedChildren,
+        __mounted__: false,
+        setParent(parent) {
+            this.parent = parent;
+        },
+        _mounting_() {
+            this.mounting?.();
+            this.children?.map((child) => {
+                child._mounting_.bind(child)();
+            });
+        },
+        async _mounted_() {
+            this.__mounted__ = true;
+            this.mounted?.();
+            this.children?.map((child) => {
+                child._mounted_.bind(child)();
+            });
+        },
+        _unmounting_() {
+            this.unmounting?.();
+            this.children?.map((child) => {
+                child._unmounting_.bind(child)();
+            });
+        },
+        async _unmounted_() {
+            this.__mounted__ = false;
+            this.unmounted?.();
+            this.children?.map((child) => {
+                child._unmounted_.bind(child)();
+            });
+        },
+    };
+
+    if (properties.className) {
+        let classes = properties.className
+            .split(" ")
+            .map((t) => {
+                return t.trim();
+            })
+            .filter((t) => {
+                return t.length > 0;
+            });
+
+        element.classList.add(...classes);
+    }
+
+    if (properties.class) {
+        let classes = properties.class
+            .split(" ")
+            .map((t) => {
+                return t.trim();
+            })
+            .filter((t) => {
+                return t.length > 0;
+            });
+
+        element.classList.add(...classes);
+    }
+
+    if (properties.style) {
+        Object.assign(element.style, properties.style);
+    }
+
+    if (properties.instanceReference) {
+        properties.instanceReference.instance = element;
+    }
+
+    Object.keys(properties).map((key) => {
+        if (
+            ["instanceReference", "style", "className", "class"].includes(key)
+        ) {
+            return;
+        }
+
+        if (isEventListener(key, properties[key])) {
+            element.addEventListener(
+                key.slice(2).toLowerCase(),
+                properties[key],
+            );
+        } else if (isValidElementProperty(key, properties[key])) {
+            try {
+                element.setAttribute(key, properties[key]);
+            } catch (e) {
+                //
+                console.error(e);
+            }
+        }
+    });
+
+    loadedChildren.map((child) => {
+        child.setParent(elementWrapper);
+        element.appendChild(child.element);
+    });
+
+    return elementWrapper;
+}
+
+/**
+ * Generates a function for custom element creation.
+ * From a wrapped custom component properties.
+ * @param properties The custom component's properties.
+ * @returns An element constructor function that takes props.
+ */
+export function CreateComponent(
+    properties: CurlUIComponentProps,
+): CurlUIComponent {
+    let component: CurlUIWrappedComponent = wrapComponent(properties);
+
+    let wrapper: CurlUIComponent = function (
+        props: CurlUIElementProps<CurlUINativeElement>,
+    ): CurlUIRenderElement {
+        return createComponentInstance(component, props);
+    };
+
+    Object.assign(wrapper, component);
+
+    return wrapper;
+}
+
+/**
+ * Constructor function for an instanceReference handle that can be
+ * used to externally refer to a component instance
+ * @returns
+ */
+export function InstanceReference() {
+    return {
+        isInstanceReference: true,
+        instance: null,
+    };
+}
+
+/**
  * Actually renders a renderable CurlUI element to the DOM element specified
  * @param element
  * @param htmlElement
  */
-function Render_(element: CurlUIRenderElement, htmlElement: HTMLElement) {
+export function Render(
+    element: CurlUIRenderElement,
+    htmlElement: HTMLElement,
+): void {
     element.setParent(htmlElement);
     htmlElement.innerHTML = "";
 
@@ -690,25 +705,14 @@ function Render_(element: CurlUIRenderElement, htmlElement: HTMLElement) {
 }
 
 /**
- * Constructor function for an instanceReference handle that can be used to externally refer to a component instance
- * @returns
- */
-function InstanceReference_() {
-    return {
-        isInstanceReference: true,
-        instance: null,
-    };
-}
-
-/**
  * Constructor for a CurlUIStore,a watchable properties object,
  * listeners can be bound to it for each time the state of the store chages.
  * @param state The initial state of the store.
  * @returns The store object.
  */
-function Store_(state: CurlUIStoreState): CurlUIStore {
+export function Store(defaultState: CurlUIStoreState): CurlUIStore {
     return {
-        state: state,
+        state: defaultState,
         handlers: {},
         getState() {
             return this.state;
@@ -724,7 +728,7 @@ function Store_(state: CurlUIStoreState): CurlUIStore {
             });
         },
         subscribe(handler: Function): string {
-            let handlerId = GetUniqueId_();
+            let handlerId = getUniqueId();
             this.handlers[handlerId] = handler;
 
             return handlerId;
@@ -733,60 +737,4 @@ function Store_(state: CurlUIStoreState): CurlUIStore {
             delete this.handlers[handlerId];
         },
     };
-}
-
-/**
- * Creates a CurlUIRenderElement, a wrapper oven an element CurlUI can render on the DOM.
- * @param tag The element tag
- * @param properties Attributes to assign to the element
- * @param children The elements child elements.
- * @returns A CurlUIRenderElement.
- */
-export function CreateElement(
-    tag: CurlUITag,
-    properties: CurlUIElementProps<CurlUINativeElement>,
-    ...children: Array<CurlUIChildComponent | Array<CurlUIChildComponent>>
-) {
-    return CreateElement_(tag, properties, ...children);
-}
-
-/**
- * Generates a function for custom element creation. From a wrapped custom component properties.
- * @param properties The custom component's properties.
- * @returns An element constructor function that takes props.
- */
-export function CreateComponent(
-    properties: CurlUIComponentProps
-): CurlUIComponent {
-    return CreateComponent_(properties);
-}
-
-/**
- * Constructor function for an instanceReference handle that can be used to externally refer to a component instance
- * @returns
- */
-export function InstanceReference() {
-    return InstanceReference_();
-}
-
-/**
- * Actually renders a renderable CurlUI element to the DOM element specified
- * @param element
- * @param htmlElement
- */
-export function Render(
-    element: CurlUIRenderElement,
-    htmlElement: HTMLElement
-): void {
-    return Render_(element, htmlElement);
-}
-
-/**
- * Constructor for a CurlUIStore,a watchable properties object,
- * listeners can be bound to it for each time the state of the store chages.
- * @param state The initial state of the store.
- * @returns The store object.
- */
-export function Store(defaultState: CurlUIStoreState): CurlUIStore {
-    return Store_(defaultState);
 }
